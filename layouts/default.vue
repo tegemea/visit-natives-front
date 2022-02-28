0e091d273442f0f4f791fdde54f94fab11d87692
lema <lemajunior@hotmail.com>
2022-02-28 20:02:25 +0300

    configured default layout to initially pull all datas

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,23 @@
     <Nuxt keep-alive />
   </div>
 </template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters(['pages','tours','countries','tourCategories','destinationCategories', 'destinations'])
+  },
+  mounted(){
+    if(!this.pages.length) this.getPages();
+    if(!this.tours.length) this.getTours();
+    if(!this.countries.length) this.getCountries();
+    if(!this.tourCategories.length) this.getTourCategories();
+    if(!this.destinationCategories.length) this.getDestinationCategories();
+    if(!this.destinations.length) this.getDestinations();
+  },
+  methods: {
+    ...mapActions(['getPages','getTours','getCountries','getTourCategories','getDestinationCategories','getDestinations'])
+  }
+}
+</script>
--- store/index.js
+++ store/index.js
@@ -31,28 +31,30 @@ export const mutations = {
 }
 
 export const actions = {
-  getPages: async ({ state, commit }) => {
-    const { data } = await this.$axios.$get(`${state.apiURL}/pages`)
+  async getPages({ state, commit }) {
+    const data = await this.$axios.$get(`${state.apiURL}/pages`)
     if(data) commit('SET_PAGES', data)
   },
-  getTours: async ({ state, commit }) => {
-    const { data } = await this.$axios.$get(`${state.apiURL}/tours`)
+  async getTours({ state, commit }) {
+    const data = await this.$axios.$get(`${state.apiURL}/tours`)
     if(data) commit('SET_TOURS', data)
   },
-  getTourCategories: async ({ state, commit }) => {
-    const { data } = await this.$axios.$get(`${state.apiURL}/tour-categories`)
+  async getTourCategories({ state, commit }) {
+    const data = await this.$axios.$get(`${state.apiURL}/tour-categories`)
     if(data) commit('SET_TOUR_CATEGORIES', data)
   },
-  getDestinationCategories: async ({ state, commit }) => {
-    const { data } = await this.$axios.$get(`${state.apiURL}/destination-categories`)
+  async getDestinationCategories({ state, commit }) {
+    console.log('here...')
+    const data = await this.$axios.$get(`${state.apiURL}/destination-categories`)
+    console.log('destination categories', data)
     if(data) commit('SET_DESTINATION_CATEGORIES', data)
   },
-  getDestinations: async ({ state, commit }) => {
-    const { data } = await this.$axios.$get(`${state.apiURL}/destinations`)
+  async getDestinations({ state, commit }) {
+    const data = await this.$axios.$get(`${state.apiURL}/destinations`)
     if(data) commit('SET_DESTINATIONS', data)
   },
-  getCountries: async ({ state, commit }) => {
-    const { data } = await this.$axios.$get(`${state.apiURL}/countries`)
+  async getCountries({ state, commit }) {
+    const data = await this.$axios.$get(`${state.apiURL}/countries`)
     if(data) commit('SET_COUNTRIES', data)
   }
 }
